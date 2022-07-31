bb8d253c53d817d992fc5993c24bb09d5b44a04a
pc <phuc16052001@gmail.com>
2022-07-31 14:17:16 +0700

    djsaiodjaiojdoiaw

--- decompose.py
+++ decompose.py
@@ -29,40 +29,54 @@
 #input n from test file
 #return result from recursive function
 
-def decompose(n):
-    result = recursive(0, n - 1, n)
-    result.reverse()
-    return result
+# def decompose(n):
+#     result = recursive(0, n - 1, n)
+#     result.reverse()
+#     return result
+#
+# def recursive(sum, x, n, result = []):
+#     #Check condition
+#     if sum == n * n:
+#         return result
+#     if x == 0:
+#         if result != None:
+#             x = result.pop(-1)
+#         else:
+#             return
+#         sum -= x * x
+#         if x == 0:
+#             return None
+#         else:
+#             return recursive(sum, x - 1, n)
+#
+#
+#     #Recursive part
+#     if check_sum_and_n(sum, x, n):
+#         sum += x * x
+#         result.append(x)
+#         return recursive(sum, x - 1, n)
+#     else:
+#         return recursive(sum, x - 1, n)
+#
+# def check_sum_and_n(sum, x, n):
+#     sum += x * x
+#     if sum <= n * n and x != 0:
+#         return True
+#     else:
+#         return False
+#
+# print(decompose(8))
 
-def recursive(sum, x, n, result = []):
-    #Check condition
-    if sum == n * n:
-        return result
-    if x == 0:
-        if result != None:
-            x = result.pop(-1)
-        else:
-            return
-        sum -= x * x
-        if x == 0:
-            return None
-        else:
-            return recursive(sum, x - 1, n)
+def decompose(n, a=None):
+    if a == None: a = n*n
+    if a == 0: return []
+    for m in range(min(n-1, int(a ** .5)), 0, -1):
+        sub = decompose(m, a - m*m)
+        if sub != None: return sub + [m]
 
+#Better solution
+#this will work until a == 0 --> sum == n --> we have our first satisfied value
+    #then it will recurse back to its root
+    #so the result list will naturally increase from bottom to top ( small to big ).
+    #clever solution, should be the one they intended to direct solver to do
 
-    #Recursive part
-    if check_sum_and_n(sum, x, n):
-        sum += x * x
-        result.append(x)
-        return recursive(sum, x - 1, n)
-    else:
-        return recursive(sum, x - 1, n)
-
-def check_sum_and_n(sum, x, n):
-    sum += x * x
-    if sum <= n * n and x != 0:
-        return True
-    else:
-        return False
-
-print(decompose(8))
