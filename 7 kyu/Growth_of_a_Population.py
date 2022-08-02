9abf300f3572b7e61a7652722bba1e75e94cd4d6
pc <phuc16052001@gmail.com>
2022-08-02 16:54:48 +0700

    djsaiodjaiojdoiaw

---
+++ 7 kyu/Growth_of_a_Population.py
@@ -0,0 +1,16 @@
+import math
+#floor not actually needed here, can use int() instead
+def nb_year(p0, percent, aug, p):
+    percentage = percent / 100
+    p_now = p0
+    year = 0
+    #p_next =
+        #p_now + p_now * percentage + aug
+    while p_now < p:
+        p_now = p_now + math.floor(p_now * percentage) + aug
+        year += 1
+    return year
+
+print(nb_year(1000, 2.0, 50, 1214))
+#solution page:
+#there are recursive function and
