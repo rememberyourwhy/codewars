e571b8d8c35438413d6b826503800f77077b2ac6
rememberyourwhy <95213333+rememberyourwhy@users.noreply.github.com>
2022-08-03 14:27:19 +0700

    Update Regex_validate_PIN_code.py

--- 7 kyu/Regex_validate_PIN_code.py
+++ 7 kyu/Regex_validate_PIN_code.py
@@ -5,3 +5,12 @@ If the function is passed a valid PIN string, return true, else return false."""
 #One line solution
 def validate_pin(pin):
     return len(pin) in (4, 6) and pin.isdigit()
+
+# Normal solution
+# def validate_pin(pin):
+#     if len pin == 4 or len pin == 6:
+#         for element in pin:
+#             if str(element) not in "1234567890":
+#                 return False
+#         return True
+#     return True
